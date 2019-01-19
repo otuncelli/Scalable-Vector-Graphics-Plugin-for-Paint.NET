181069654a01ed0ffeb7a3c49a2fcff9ff3405b2
Ievgen Naida <evgenynaida@gmail.com>
2019-01-20 00:42:58 +0100

    Background color is changed.

--- SvgFileType/UIDialog.Designer.cs
+++ SvgFileType/UIDialog.Designer.cs
@@ -530,6 +530,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(310, 403);
             this.Controls.Add(this.lbProgress);
             this.Controls.Add(this.progress);
